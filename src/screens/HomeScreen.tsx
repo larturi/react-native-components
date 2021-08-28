07618c4f2da9bc1360edadf898d438a6a929f0ce
lea.arturi <lea.arturi@gmail.com>
2021-08-28 15:06:35 -0300

    Instalacion Icons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Icon name="star-outline" size={50} color="black" />
     </View>
   );
 };
