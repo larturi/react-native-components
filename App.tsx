71385ddf44e6ff787fce309876c15b39a61360b2
lea.arturi <lea.arturi@gmail.com>
2021-08-28 14:52:17 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {Navigator} from './src/navigator/Navigator';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
